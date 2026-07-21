5cab1f726cfafe691d1424166a24d6161dce3f08
mari <marianarubertgrecco@gmail.com>
2026-07-21 09:56:15 -0300

    feat(clients): complete frontend integration with backend API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,15 @@
+import cors from 'cors';
 import express from 'express';
 
 import { routes } from './routes';
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  }),
+);
 
 app.use(express.json());
 
